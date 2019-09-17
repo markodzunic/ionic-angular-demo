754a72ba35325f391831f0687f620f3bf4d632ae
marko <soul6reaver@gmail.com>
2019-09-17 11:15:59 +0200

    some more implementations

--- src/app/recipes/recipes-details/recipes-details.module.ts
+++ src/app/recipes/recipes-details/recipes-details.module.ts
@@ -5,13 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { RecipesPage } from './recipes.page';
-import {RecipesItemComponent} from './recipes-item/recipes-item.component';
+import { RecipesDetailsPage } from './recipes-details.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: RecipesPage
+    component: RecipesDetailsPage
   }
 ];
 
@@ -22,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [RecipesPage, RecipesItemComponent]
+  declarations: [RecipesDetailsPage]
 })
-export class RecipesPageModule {}
+export class RecipesDetailsPageModule {}
---
+++ src/app/recipes/recipes-item/recipes-item.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipesItemComponent } from './recipes-item.component';
+
+describe('RecipesItemComponent', () => {
+  let component: RecipesItemComponent;
+  let fixture: ComponentFixture<RecipesItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipesItemComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipesItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

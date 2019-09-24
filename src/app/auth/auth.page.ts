d3d6ab8adfc767da24be037f3fbf7256156b94cc
marko <soul6reaver@gmail.com>
2019-09-24 12:55:43 +0200

    user input changes

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import {LoadingController} from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService,
               private router: Router,
@@ -33,5 +35,23 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     });
+  }
+
+    onSubmit(f: NgForm) {
+      if (!f.valid) {
+        return;
+      }
+      const email = f.value.email;
+      const password = f.value.password;
+
+      if (this.isLogin) {
+
+      } else {
+
+      }
+    }
+
+  onSwitchAuth() {
+    this.isLogin = !this.isLogin;
   }
 }
